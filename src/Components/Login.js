e2a074cc4bacf57e14ff1ec4b7f6c205da2853b7
DavidRios22 <davidriosdoeswork@gmail.com>
2023-05-11 18:03:19 -0500

    added functionality with clientside

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import { useState } from 'react'
+import Box from "@mui/material/Box"
+import TextField from "@mui/material/TextField"
 
 export default function Login() {
   return (
-    <div>
-        
-    </div>
+    <Box
+      component="form"
+      sx={{
+        "& > :not(style)": { m: 1, width: "25ch" },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="username" label="Standard" variant="standard" />
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+    </Box>
   )
 }
--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,7 +1,65 @@
-import React from 'react'
+import React from "react"
+import { useState } from "react"
+import Axios from "axios"
+import Box from "@mui/material/Box"
+import TextField from "@mui/material/TextField"
+import Button from "@mui/material/Button"
+
 
 export default function Register() {
+  // let username = req.body.username
+  // let password = req.body.password
+  // let firstName = req.body.firstName
+
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [name, setName] = useState("")
+
+  const addUser = () => {
+    Axios.post("http://localhost:4001/register", {
+      username: username,
+      password: password,
+      firstName: name
+    }).then(() => {
+      console.log("success");
+    })
+  }
+
+
   return (
-    <div>Register</div>
+    <Box
+      component="form"
+      sx={{
+        "& > :not(style)": { m: 1, width: "25ch" },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField
+        id="Username"
+        label="Username"
+        variant="standard"
+        onChange={(e) => {
+          setUsername(e.target.value)
+        }}
+      />
+      <TextField
+        id="Password"
+        label="Password"
+        variant="standard"
+        onChange={(e) => {
+          setPassword(e.target.value)
+        }}
+      />
+      <TextField
+        id="Name"
+        label="Name"
+        variant="standard"
+        onChange={(e) => {
+          setName(e.target.value)
+        }}
+      />
+      <Button onClick={addUser} variant="text">Register</Button>
+    </Box>
   )
 }
