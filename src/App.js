e1df92ccf3b22f3865b9389575ef27b678e32da0
DavidRios22 <davidriosdoeswork@gmail.com>
2023-06-13 21:20:27 -0500

    hashrouter implemented

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import "./App.css"
 import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
-import Router from "./Router"
+import Router from "./Router.js"
 import store from "./Redux/store"
-// import Navigation from "./Components/Navigation"
-
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
         {/* <Navigation /> */}
         <Router />
-      </BrowserRouter>
     </Provider>
   )
 }
--- src/Router.js
+++ src/Router.js
@@ -1,3 +1,22 @@
+// import { Routes, Route } from "react-router-dom"
+// import Home from "./Components/Home"
+// import Register from "./Components/Register"
+// import Weights from "./Components/Weights"
+// import Habits from "./Components/Habits"
+
+// const Router = () => {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Home />} />
+//       <Route path="/register" element={<Register />} />
+//       <Route path="/weight" element={<Weights />} />
+//       <Route path="/habits" element={<Habits/>} />
+//     </Routes>
+//   )
+// }
+
+// export default Router
+
 import { Routes, Route } from "react-router-dom"
 import Home from "./Components/Home"
 import Register from "./Components/Register"
@@ -16,3 +35,4 @@ const Router = () => {
 }
 
 export default Router
+
